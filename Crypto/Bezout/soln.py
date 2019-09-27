c7cee1c9906cf20a2c9780bb70dd7df7d1c34338
NyxTo <44107187+NyxTo@users.noreply.github.com>
2019-09-27 11:15:01 +0800

    Update soln.py

--- Crypto/Bezout/soln.py
+++ Crypto/Bezout/soln.py
@@ -27,7 +27,7 @@ d = gen_bezout(e)
 m = 1
 for i in range(20):
     if d[i] < 0:
-		c[i], _, _ = ext_euclid(c[i], n)
+        c[i], _, _ = ext_euclid(c[i], n)
     m = m * pow(c[i], abs(d[i]), n) % n
 
 print hex(m)[2:].decode('hex')
